5a8795927fd14cc4c1ee8fc1902c1c733aa5da12
Darri Valgarðsson <DarrMac@Darris-MacBook-Air.local>
2016-10-14 11:58:51 +0000

    refactoraði, if lykkjan einfölduð

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,15 +5,13 @@ public class Calculator {
 	public static int add(String text){
 		if(text == ""){
         return 0;
-			}else if(text.contains(",")){
+			}else{
 				String [] fields = text.split(",");
 				int sum = 0;
 				for(int i=0; i<fields.length; i++){
 					sum+=Integer.parseInt(fields[i]);
 				}
 				return sum;
-			}else{
-				return Integer.parseInt(text);
 			}
 	}
 
